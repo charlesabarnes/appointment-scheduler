e81300a90ea40e140896583f5db7909f2854fa67
Charles Barnes <charles@charlesabarnes.com>
2020-05-04 06:57:04 -0500

    add confirm page

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm.component.scss']
 })
 export class ConfirmComponent implements OnInit {
+  value;
+  times = ['11:30', '12:00', '12:30'];
+  private selectedTimes = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isSelected(time: string): boolean {
+    return this.selectedTimes.indexOf(time) > -1;
+  }
+
+  select(time) {
+    if(this.isSelected(time)) {
+      this.selectedTimes.splice(this.selectedTimes.indexOf(time), 1);
+      
+    } else {
+      this.selectedTimes.push(time);
+    }
   }
 
 }
